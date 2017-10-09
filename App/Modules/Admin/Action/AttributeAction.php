b90a375ed553f8899390b714c44b98c99dda3cf9
Administrator <1880161@qq.com>
2017-10-09 19:19:30 +0800

    111

---
+++ App/Modules/Admin/Action/AttributeAction.php
@@ -0,0 +1,18 @@
+<?php
+
+class AttributeAction extends CommonAction{
+
+    public function index(){
+        $this->display();
+    }
+
+    public function addAttr(){
+        p('111');
+        die;
+        $this->display();
+    }
+
+    public function addAttrHandle(){
+
+    }
+}
